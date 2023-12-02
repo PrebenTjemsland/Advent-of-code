5101972355d1738ce8dd9b8ebb878f1855d96f6a
PtjeEquinor <ptje@equinor.com>
2023-12-02 19:44:10 +0100

    Remove logs

--- AdventOfCode2023/day01/Task1.py
+++ AdventOfCode2023/day01/Task1.py
@@ -8,8 +8,6 @@ with open('Dataset.txt') as f:
         if not line:
             break
         nums = ar = re.findall('[0-9]', line)
-        print(nums)
-        print(int(nums[0] + nums[-1]))
         calibrationValue += int(nums[0] + nums[-1])
 
 print(calibrationValue)
